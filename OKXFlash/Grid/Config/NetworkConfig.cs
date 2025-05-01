aaf1c03ce516be37b6656a2bef44541f6938faf9
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:58 +0300

    NetworkConfig.cs

---
+++ OKXFlash/Grid/Config/NetworkConfig.cs
@@ -0,0 +1,12 @@
+﻿/*
+ * Copyright (C) 2024 The Hong-Jin Investment Company.
+ * This file is part of the OKX Trading Server.
+ * File created at 2024-12-05
+ */
+namespace HongJinInvestment.OKX.Server;
+
+public static class NetworkConfig
+{
+    public static readonly int    Port = 38388;
+    public static readonly string BaseUrl = $"https://www.okx.com";
+}
