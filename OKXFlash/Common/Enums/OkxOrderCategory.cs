84e0da48653d3feea5d0a0483e4ec583a9dfc5ee
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:40 +0300

    OkxOrderCategory.cs

---
+++ OKXFlash/Common/Enums/OkxOrderCategory.cs
@@ -0,0 +1,49 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// OKX Order Category
+/// </summary>
+public enum OkxOrderCategory
+{
+    /// <summary>
+    /// Normal
+    /// </summary>
+    [Map("normal")]
+    Normal = 1,
+
+    /// <summary>
+    /// TWAP
+    /// </summary>
+    [Map("twap")]
+    TWAP = 2,
+
+    /// <summary>
+    /// ADL
+    /// </summary>
+    [Map("adl")]
+    ADL = 3,
+
+    /// <summary>
+    /// FullLiquidation
+    /// </summary>
+    [Map("full_liquidation")]
+    FullLiquidation = 4,
+
+    /// <summary>
+    /// PartialLiquidation
+    /// </summary>
+    [Map("partial_liquidation")]
+    PartialLiquidation = 5,
+
+    /// <summary>
+    /// Delivery
+    /// </summary>
+    [Map("delivery")]
+    Delivery = 6,
+
+    /// <summary>
+    /// DDH
+    /// </summary>
+    [Map("ddh")]
+    DDH = 7,
+}
