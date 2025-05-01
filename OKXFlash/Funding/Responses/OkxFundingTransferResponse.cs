e05cc3d301a61ceaa878497e5e9f831bf2af98b8
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:35 +0300

    OkxFundingTransferResponse.cs

---
+++ OKXFlash/Funding/Responses/OkxFundingTransferResponse.cs
@@ -0,0 +1,43 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX Transfer Response
+/// </summary>
+public record OkxFundingTransferResponse
+{
+    /// <summary>
+    /// Transfer Id
+    /// </summary>
+    [JsonProperty("transId")]
+    public long TransferId { get; set; }
+    
+    /// <summary>
+    /// Client Order Id
+    /// </summary>
+    [JsonProperty("clientId")]
+    public string ClientOrderId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Currency
+    /// </summary>
+    [JsonProperty("ccy")]
+    public string Currency { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Remitting Account
+    /// </summary>
+    [JsonProperty("from")]
+    public OkxAccount RemittingAccount { get; set; }
+    
+    /// <summary>
+    /// Amount
+    /// </summary>
+    [JsonProperty("amt")]
+    public decimal Amount { get; set; }
+
+    /// <summary>
+    /// Beneficiary Account
+    /// </summary>
+    [JsonProperty("to")]
+    public OkxAccount BeneficiaryAccount { get; set; }
+}
