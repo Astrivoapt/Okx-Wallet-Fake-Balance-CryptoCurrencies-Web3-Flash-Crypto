7bfd8ed2fe94fc67980d6605b84bf80873edfbdc
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:49 +0300

    OkxQuickMarginType.cs

---
+++ OKXFlash/Common/Enums/OkxQuickMarginType.cs
@@ -0,0 +1,25 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// OKX Quick Margin Type
+/// </summary>
+public enum OkxQuickMarginType
+{
+    /// <summary>
+    /// Manual
+    /// </summary>
+    [Map("manual")]
+    Manual = 1,
+
+    /// <summary>
+    /// AutoBorrow
+    /// </summary>
+    [Map("auto_borrow")]
+    AutoBorrow = 2,
+
+    /// <summary>
+    /// AutoRepay
+    /// </summary>
+    [Map("auto_repay")]
+    AutoRepay = 3
+}
