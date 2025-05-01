5208b1475e05257ce992f63afe0bdfcbc7eec395
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:42 +0300

    OkxFundingWithdrawalResponse.cs

---
+++ OKXFlash/Funding/Responses/OkxFundingWithdrawalResponse.cs
@@ -0,0 +1,37 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX Withdrawal Response
+/// </summary>
+public record OkxFundingWithdrawalResponse
+{
+    /// <summary>
+    /// Currency
+    /// </summary>
+    [JsonProperty("ccy")]
+    public string Currency { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Chain
+    /// </summary>
+    [JsonProperty("chain")]
+    public string Chain { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Amount
+    /// </summary>
+    [JsonProperty("amt")]
+    public decimal Amount { get; set; }
+
+    /// <summary>
+    /// Withdrawal Id
+    /// </summary>
+    [JsonProperty("wdId")]
+    public long WithdrawalId { get; set; }
+
+    /// <summary>
+    /// Client Order Id
+    /// </summary>
+    [JsonProperty("clientId")]
+    public string ClientOrderId { get; set; } = string.Empty;
+}
