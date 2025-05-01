884a45bfcc90d065c9dcb6de677f1acdca704996
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:10:20 +0300

    OkxFinancialStakingType.cs

---
+++ OKXFlash/Financial/Enums/OkxFinancialStakingType.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Financial;
+
+/// <summary>
+/// OKX Financial Eth Staking Type
+/// </summary>
+public enum OkxFinancialStakingType
+{
+    /// <summary>
+    /// Redeem
+    /// </summary>
+    [Map("purchase")]
+    Purchase = 1,
+
+    /// <summary>
+    /// Redeem
+    /// </summary>
+    [Map("redeem")]
+    Redeem = 2,
+}
