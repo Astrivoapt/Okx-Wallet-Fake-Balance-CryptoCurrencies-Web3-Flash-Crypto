a5536517094228e2ae236db74a3ddf9bc7d949bd
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:13:29 +0300

    OkxRecurringBuyItem.cs

---
+++ OKXFlash/RecurringBuy/Responses/OkxRecurringBuyItem.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.RecurringBuy;
+
+/// <summary>
+/// Recurring Buy Item
+/// </summary>
+public record OkxRecurringBuyItem
+{
+    /// <summary>
+    /// Recurring currency, e.g. BTC
+    /// </summary>
+    [JsonProperty("ccy")]
+    public string Currency { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Proportion of recurring currency assets, e.g. "0.2" representing 20%
+    /// </summary>
+    [JsonProperty("ratio")]
+    public string Ratio { get; set; } = string.Empty;
+}
