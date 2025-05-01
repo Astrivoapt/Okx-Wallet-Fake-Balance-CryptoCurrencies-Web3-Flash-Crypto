df1496517b9a26884174a7fe14682be18d9f3b43
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:05 +0300

    OkxGridAlgoSubOrderType.cs

---
+++ OKXFlash/Grid/Enums/OkxGridAlgoSubOrderType.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Algo Sub-Order Type
+/// </summary>
+public enum OkxGridAlgoSubOrderType
+{
+    /// <summary>
+    /// Live
+    /// </summary>
+    [Map("live")]
+    Live = 1,
+
+    /// <summary>
+    /// Filled
+    /// </summary>
+    [Map("filled")]
+    Filled = 2,
+}
