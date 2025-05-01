0f9241287456d9cf6ebe04fcb7aa227b7732a9a0
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:14:06 +0300

    OkxTradeClosePositionResponse.cs

---
+++ OKXFlash/Trade/Responses/OkxTradeClosePositionResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace OKX.Api.Trade;
+
+/// <summary>
+/// OKX Close Position Response
+/// </summary>
+public record OkxTradeClosePositionResponse
+{
+    /// <summary>
+    /// Instrument ID
+    /// </summary>
+    [JsonProperty("instId")]
+    public string InstrumentId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Position Side
+    /// </summary>
+    [JsonProperty("posSide")]
+    public OkxTradePositionSide PositionSide { get; set; }
+
+    /// <summary>
+    /// Client-supplied ID
+    /// A combination of case-sensitive alphanumerics, all numbers, or all letters of up to 32 characters.
+    /// </summary>
+    [JsonProperty("clOrdId")]
+    public string ClientOrderId { get; set; } = string.Empty;
+}
