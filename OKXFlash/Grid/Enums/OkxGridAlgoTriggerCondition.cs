2e57de20dadd8415cea6f7a68e19fdd1aa4fc039
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:10 +0300

    OkxGridAlgoTriggerCondition.cs

---
+++ OKXFlash/Grid/Enums/OkxGridAlgoTriggerCondition.cs
@@ -0,0 +1,37 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Algo Trigger Condition
+/// </summary>
+public enum OkxGridAlgoTriggerCondition
+{
+    /// <summary>
+    /// CrossUp
+    /// </summary>
+    [Map("cross_up")]
+    CrossUp = 1,
+
+    /// <summary>
+    /// CrossDown
+    /// </summary>
+    [Map("cross_down")]
+    CrossDown = 2,
+
+    /// <summary>
+    /// Above
+    /// </summary>
+    [Map("above")]
+    Above = 3,
+
+    /// <summary>
+    /// Below
+    /// </summary>
+    [Map("below")]
+    Below = 4,
+
+    /// <summary>
+    /// Cross
+    /// </summary>
+    [Map("cross")]
+    Cross = 5
+}
