b8ae7cea738f5e35f09133dd1de205a1bbca61dd
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:30 +0300

    OkxInstrumentState.cs

---
+++ OKXFlash/Common/Enums/OkxInstrumentState.cs
@@ -0,0 +1,25 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// OKX Instrument State
+/// </summary>
+public enum OkxInstrumentState
+{
+    /// <summary>
+    /// Live
+    /// </summary>
+    [Map("live")]
+    Live = 1,
+
+    /// <summary>
+    /// Suspend
+    /// </summary>
+    [Map("suspend")]
+    Suspend = 2,
+
+    /// <summary>
+    /// PreOpen
+    /// </summary>
+    [Map("preopen")]
+    PreOpen = 3,
+}
