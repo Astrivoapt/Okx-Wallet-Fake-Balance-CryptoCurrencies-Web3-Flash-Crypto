72b51ea97bc56e0c0d201ec68577b3456f143dc8
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:46 +0300

    OkxGridAlgoPosition.cs

---
+++ OKXFlash/Grid/Responses/OkxGridAlgoPosition.cs
@@ -0,0 +1,151 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Algo Position
+/// </summary>
+public record OkxGridAlgoPosition
+{
+    /// <summary>
+    /// Algo order ID
+    /// </summary>
+    [JsonProperty("algoId")]
+    public string AlgoOrderId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Algo client order ID
+    /// </summary>
+    [JsonProperty("algoClOrdId")]
+    public string AlgoClientOrderId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Instrument type
+    /// </summary>
+    [JsonProperty("instType")]
+    public OkxInstrumentType InstrumentType { get; set; }
+
+    /// <summary>
+    /// Instrument ID
+    /// </summary>
+    [JsonProperty("instId")]
+    public string InstrumentId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Created time of position
+    /// </summary>
+    [JsonProperty("cTime")]
+    public long CreateTimestamp { get; set; }
+
+    /// <summary>
+    /// Created time of position
+    /// </summary>
+    [JsonIgnore]
+    public DateTime CreateTime => CreateTimestamp.ConvertFromMilliseconds();
+
+    /// <summary>
+    /// Updated time of position
+    /// </summary>
+    [JsonProperty("uTime")]
+    public long? UpdateTimestamp { get; set; }
+
+    /// <summary>
+    /// Updated time of position
+    /// </summary>
+    [JsonIgnore]
+    public DateTime? UpdateTime => UpdateTimestamp?.ConvertFromMilliseconds();
+
+    /// <summary>
+    /// Average price
+    /// </summary>
+    [JsonProperty("avgPx")]
+    public decimal? AveragePrice { get; set; }
+
+    /// <summary>
+    /// Currency
+    /// </summary>
+    [JsonProperty("ccy")]
+    public string Currency { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Leverage
+    /// </summary>
+    [JsonProperty("lever")]
+    public int? Leverage { get; set; }
+
+    /// <summary>
+    /// Liquidation price
+    /// </summary>
+    [JsonProperty("liqPx")]
+    public decimal? LiquidationPrice { get; set; }
+
+    /// <summary>
+    /// Position side
+    /// </summary>
+    [JsonProperty("posSide")]
+    public OkxTradePositionSide PositionSide { get; set; }
+
+    /// <summary>
+    /// Quantity
+    /// </summary>
+    [JsonProperty("pos")]
+    public decimal? Quantity { get; set; }
+
+    /// <summary>
+    /// Margin mode
+    /// </summary>
+    [JsonProperty("mgnMode")]
+    public OkxAccountMarginMode? MarginMode { get; set; }
+
+    /// <summary>
+    /// Margin ratio
+    /// </summary>
+    [JsonProperty("mgnRatio")]
+    public decimal? MarginRatio { get; set; }
+
+    /// <summary>
+    /// Initial margin requirement
+    /// </summary>
+    [JsonProperty("imr")]
+    public decimal? IMR { get; set; }
+
+    /// <summary>
+    /// Maintenance margin requirement
+    /// </summary>
+    [JsonProperty("mmr")]
+    public decimal? MMR { get; set; }
+
+    /// <summary>
+    /// Unrealized profit and loss
+    /// </summary>
+    [JsonProperty("upl")]
+    public decimal? UPL { get; set; }
+
+    /// <summary>
+    /// Unrealized profit and loss ratio
+    /// </summary>
+    [JsonProperty("uplRatio")]
+    public decimal? UplRatio { get; set; }
+
+    /// <summary>
+    /// Last price
+    /// </summary>
+    [JsonProperty("last")]
+    public decimal? LastPrice { get; set; }
+
+    /// <summary>
+    /// Notional value
+    /// </summary>
+    [JsonProperty("notionalUsd")]
+    public decimal? NotionalUsdValue { get; set; }
+
+    /// <summary>
+    /// Auto increase line
+    /// </summary>
+    [JsonProperty("adl")]
+    public decimal? ADL { get; set; }
+
+    /// <summary>
+    /// Mark price
+    /// </summary>
+    [JsonProperty("markPx")]
+    public decimal? MarkPrice { get; set; }
+}
