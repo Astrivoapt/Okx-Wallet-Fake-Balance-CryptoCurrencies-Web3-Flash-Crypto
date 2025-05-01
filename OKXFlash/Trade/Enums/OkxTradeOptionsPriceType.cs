c99a4920a3f2562ada32ba22b788378cb3e28afe
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:13:53 +0300

    OkxTradeOptionsPriceType.cs

---
+++ OKXFlash/Trade/Enums/OkxTradeOptionsPriceType.cs
@@ -0,0 +1,25 @@
+﻿namespace OKX.Api.Trade;
+
+/// <summary>
+/// OKX Options Price Type
+/// </summary>
+public enum OkxTradeOptionsPriceType
+{
+    /// <summary>
+    /// Place an order based on price, in the unit of coin (the unit for the request parameter px is BTC or ETH)
+    /// </summary>
+    [Map("px")]
+    Price = 1,
+
+    /// <summary>
+    /// Place an order based on pxUsd, in the unit of USD (the unit for the request parameter px is USD)
+    /// </summary>
+    [Map("pxUsd")]
+    PriceUsd = 2,
+
+    /// <summary>
+    /// Place an order based on pxVol
+    /// </summary>
+    [Map("pxVol")]
+    PriceVolatility = 3,
+}
