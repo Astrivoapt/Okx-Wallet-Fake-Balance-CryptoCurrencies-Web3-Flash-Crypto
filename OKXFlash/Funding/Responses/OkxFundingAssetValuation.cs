7bf8c00e58eff4f614a6aba77b422cdc43ceb321
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:11 +0300

    OkxFundingAssetValuation.cs

---
+++ OKXFlash/Funding/Responses/OkxFundingAssetValuation.cs
@@ -0,0 +1,31 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX Asset Valuation
+/// </summary>
+public record OkxFundingAssetValuation
+{
+    /// <summary>
+    /// Total Balance
+    /// </summary>
+    [JsonProperty("totalBal")]
+    public decimal TotalBalance { get; set; }
+
+    /// <summary>
+    /// Timestamp
+    /// </summary>
+    [JsonProperty("ts")]
+    public long Timestamp { get; set; }
+
+    /// <summary>
+    /// Time
+    /// </summary>
+    [JsonIgnore]
+    public DateTime Time => Timestamp.ConvertFromMilliseconds();
+
+    /// <summary>
+    /// Details
+    /// </summary>
+    [JsonProperty("details")]
+    public OkxFundingAssetValuationDetails? Details { get; set; }
+}
