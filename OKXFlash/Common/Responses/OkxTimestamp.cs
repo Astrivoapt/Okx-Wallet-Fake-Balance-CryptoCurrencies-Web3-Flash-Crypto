9c813457d413b69260a74f17aac3dc5a67a9e58c
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:59 +0300

    OkxTimestamp.cs

---
+++ OKXFlash/Common/Responses/OkxTimestamp.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// OKX Timestamp
+/// </summary>
+public record OkxTimestamp
+{
+    /// <summary>
+    /// Timestamp
+    /// </summary>
+    [JsonProperty("ts")]
+    public long Timestamp { get; set; }
+
+    /// <summary>
+    /// Time
+    /// </summary>
+    [JsonIgnore]
+    public DateTime Time => Timestamp.ConvertFromMilliseconds();
+}
