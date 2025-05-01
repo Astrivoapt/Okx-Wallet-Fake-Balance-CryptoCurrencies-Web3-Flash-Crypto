bc0109d912040000e1acf32a77bf0bf505d8e93e
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:09 +0300

    OkxFundingWithdrawalIdContainer.cs

---
+++ OKXFlash/Funding/Internal/OkxFundingWithdrawalIdContainer.cs
@@ -0,0 +1,13 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX Withdrawal Id
+/// </summary>
+internal record OkxFundingWithdrawalIdContainer
+{
+    /// <summary>
+    /// Withdrawal Id
+    /// </summary>
+    [JsonProperty("wdId")]
+    public long Payload { get; set; }
+}
