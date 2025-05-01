1f2f45047bc9561251148e157031a4314f7aeecf
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:52 +0300

    OkxBooleanResponse.cs

---
+++ OKXFlash/Common/Responses/OkxBooleanResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// Okx Boolean Response
+/// </summary>
+public record OkxBooleanResponse
+{
+    /// <summary>
+    /// Result
+    /// </summary>
+    [JsonProperty("result")]
+    public bool Result { get; set; }
+}
