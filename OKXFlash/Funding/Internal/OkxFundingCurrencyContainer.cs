c9e3ea3b81d9a567c3064ef0ed29605371a0e179
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:08 +0300

    OkxFundingCurrencyContainer.cs

---
+++ OKXFlash/Funding/Internal/OkxFundingCurrencyContainer.cs
@@ -0,0 +1,13 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX Convert Currency
+/// </summary>
+internal record OkxFundingCurrencyContainer
+{
+    /// <summary>
+    /// Currency, e.g. BTC
+    /// </summary>
+    [JsonProperty("ccy")]
+    public string Payload { get; set; } = string.Empty;
+}
