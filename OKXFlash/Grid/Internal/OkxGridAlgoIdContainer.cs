86aab92905c5b765395b7e71a4efa956da4cd9c0
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:29 +0300

    OkxGridAlgoIdContainer.cs

---
+++ OKXFlash/Grid/Internal/OkxGridAlgoIdContainer.cs
@@ -0,0 +1,13 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Investment Add Response
+/// </summary>
+internal record OkxGridAlgoIdContainer
+{
+    /// <summary>
+    /// Algo Order Id
+    /// </summary>
+    [JsonProperty("algoId")]
+    public long? Payload { get; set; }
+}
