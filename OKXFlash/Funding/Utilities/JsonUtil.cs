3259945a5e1af87b4f1531e873a213cb1bf4e675
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:47 +0300

    JsonUtil.cs

---
+++ OKXFlash/Funding/Utilities/JsonUtil.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Copyright (C) 2024 The Hong-Jin Investment Company.
+ * This file is part of the OKX Trading Server.
+ * File created at 2024-12-05
+ */
+namespace HongJinInvestment.OKX.Server;
+
+using Newtonsoft.Json.Linq;
+
+public static class JsonUtil
+{
+    public static JObject TryPraseOkxResultJson(string json)
+    {
+        JObject jsonRoot = JObject.Parse(json);
+        string code = jsonRoot["code"].ToString();
+        if (code != "0")
+        {
+            string msg = jsonRoot["msg"].ToString();
+            LogManager.Instance.LogError($"Invalid response failed, reason: {msg}");
+            return null;
+        }
+
+        return jsonRoot;
+    }
+}
