4d9685edbd63400adbd3fe7878d956a5c0b20577
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:22 +0300

    OkxGridRunType.cs

---
+++ OKXFlash/Grid/Enums/OkxGridRunType.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Run Type
+/// </summary>
+public enum OkxGridRunType
+{
+    /// <summary>
+    /// Arithmetic
+    /// </summary>
+    [Map("1")]
+    Arithmetic = 1,
+
+    /// <summary>
+    /// Geometric
+    /// </summary>
+    [Map("2")]
+    Geometric = 2,
+}
