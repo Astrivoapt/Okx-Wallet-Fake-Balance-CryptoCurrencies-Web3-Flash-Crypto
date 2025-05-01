aa889775f7600971333253345f6015731d4bbdfb
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:51 +0300

    PathUtil.cs

---
+++ OKXFlash/Funding/Utilities/PathUtil.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Copyright (C) 2024 The Hong-Jin Investment Company.
+ * This file is part of the OKX Trading Server.
+ * File created at 2024-12-05
+ */
+namespace HongJinInvestment.OKX.Server;
+
+public static class PathUtil
+{
+    public static string SerializedDataSavePath
+    {
+        get
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),AppConfig.AppDocFolderName);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            return path;
+        }
+    }
+}
