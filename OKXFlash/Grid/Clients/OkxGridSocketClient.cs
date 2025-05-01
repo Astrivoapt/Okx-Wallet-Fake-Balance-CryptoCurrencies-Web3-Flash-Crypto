81e80adf073b6673b263766f9c997f678f946581
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:54 +0300

    OkxGridSocketClient.cs

---
+++ OKXFlash/Grid/Clients/OkxGridSocketClient.cs
@@ -0,0 +1,16 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX WebSocket Api Grid Trading Client
+/// </summary>
+public class OkxGridSocketClient(OkxWebSocketApiClient root)
+{
+    // Internal
+    internal OkxWebSocketApiClient _ { get; } = root;
+
+    // TODO: WS / Spot grid algo orders channel
+    // TODO: WS / Contract grid algo orders channel
+    // TODO: WS / Grid positions channel
+    // TODO: WS / Grid sub orders channel
+
+}
