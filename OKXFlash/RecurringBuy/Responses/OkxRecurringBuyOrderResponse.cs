13775599deaeda65b167745f9a33990335682e07
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:13:37 +0300

    OkxRecurringBuyOrderResponse.cs

---
+++ OKXFlash/RecurringBuy/Responses/OkxRecurringBuyOrderResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.RecurringBuy;
+
+/// <summary>
+/// Recurring Buy Order Response
+/// </summary>
+public record OkxRecurringBuyOrderResponse : OkxRestApiErrorBase
+{
+    /// <summary>
+    /// Algo ID
+    /// </summary>
+    [JsonProperty("algoId")]
+    public long OrderId { get; set; }
+
+    /// <summary>
+    /// Client-supplied Algo ID
+    /// </summary>
+    [JsonProperty("algoClOrdId")]
+    public string ClientOrderId { get; set; } = string.Empty;
+}
