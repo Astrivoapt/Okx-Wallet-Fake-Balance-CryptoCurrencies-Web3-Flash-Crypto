8cda40ce40fb333267a95940f2d49ec4747ef5f1
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:13:59 +0300

    OkxTradeOrderCancelRequest.cs

---
+++ OKXFlash/Trade/Requests/OkxTradeOrderCancelRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace OKX.Api.Trade;
+
+/// <summary>
+/// OKX Order Cancel Request
+/// </summary>
+public record OkxTradeOrderCancelRequest
+{
+    /// <summary>
+    /// Instrument Id
+    /// </summary>
+    [JsonProperty("instId")]
+    public string? InstrumentId { get; set; }
+
+    /// <summary>
+    /// Order Id
+    /// </summary>
+    [JsonProperty("ordId", NullValueHandling = NullValueHandling.Ignore)]
+    public long? OrderId { get; set; }
+
+    /// <summary>
+    /// Client Order Id
+    /// </summary>
+    [JsonProperty("clOrdId", NullValueHandling = NullValueHandling.Ignore)]
+    public string? ClientOrderId { get; set; }
+}
