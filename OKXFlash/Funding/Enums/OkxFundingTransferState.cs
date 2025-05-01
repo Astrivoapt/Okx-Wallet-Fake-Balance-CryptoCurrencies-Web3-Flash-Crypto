7dee97ba428dbd2355d23b241f84d49a2d86ad98
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:10:59 +0300

    OkxFundingTransferState.cs

---
+++ OKXFlash/Funding/Enums/OkxFundingTransferState.cs
@@ -0,0 +1,25 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX Transfer State
+/// </summary>
+public enum OkxFundingTransferState
+{
+    /// <summary>
+    /// Pending
+    /// </summary>
+    [Map("pending")]
+    Pending = 1,
+
+    /// <summary>
+    /// Success
+    /// </summary>
+    [Map("success")]
+    Success = 2,
+
+    /// <summary>
+    /// Failed
+    /// </summary>
+    [Map("failed")]
+    Failed = 3,
+}
