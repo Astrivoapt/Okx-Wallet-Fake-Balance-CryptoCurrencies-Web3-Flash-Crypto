d04e741f13f2bf82d1e062b48f8652b61c870cca
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:13:51 +0300

    OkxTradeOneClickRepayOrderStatus.cs

---
+++ OKXFlash/Trade/Enums/OkxTradeOneClickRepayOrderStatus.cs
@@ -0,0 +1,25 @@
+﻿namespace OKX.Api.Trade;
+
+/// <summary>
+/// OKX One Click Repay Order Status
+/// </summary>
+public enum OkxTradeOneClickRepayOrderStatus
+{
+    /// <summary>
+    /// Running
+    /// </summary>
+    [Map("running")]
+    Running = 1,
+
+    /// <summary>
+    /// Filled
+    /// </summary>
+    [Map("filled")]
+    Filled = 2,
+
+    /// <summary>
+    /// Failed
+    /// </summary>
+    [Map("failed")]
+    Failed = 3,
+}
