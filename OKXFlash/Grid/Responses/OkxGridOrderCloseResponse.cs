c1b044e9564e6540170ef2ff8c9c7e3ffc46bd48
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:58 +0300

    OkxGridOrderCloseResponse.cs

---
+++ OKXFlash/Grid/Responses/OkxGridOrderCloseResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Order Response
+/// </summary>
+public record OkxGridOrderCloseResponse : OkxRestApiErrorBase
+{
+    /// <summary>
+    /// Algo Order Id
+    /// </summary>
+    [JsonProperty("algoId")]
+    public string AlgoOrderId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Algo Client Order Id
+    /// </summary>
+    [JsonProperty("algoClOrdId")]
+    public string AlgoClientOrderId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Close position order ID
+    /// If mktClose is true, the parameter will return "".
+    /// </summary>
+    [JsonProperty("ordId")]
+    public long? OrderId { get; set; }
+}
