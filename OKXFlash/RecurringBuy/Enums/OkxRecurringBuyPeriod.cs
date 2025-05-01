171d087412c96e9752b69ad31938884cef856eb1
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:13:23 +0300

    OkxRecurringBuyPeriod.cs

---
+++ OKXFlash/RecurringBuy/Enums/OkxRecurringBuyPeriod.cs
@@ -0,0 +1,31 @@
+﻿namespace OKX.Api.RecurringBuy;
+
+/// <summary>
+/// OKX Recurring Buy Period
+/// </summary>
+public enum OkxRecurringBuyPeriod
+{
+    /// <summary>
+    /// Monthly
+    /// </summary>
+    [Map("monthly")]
+    Monthly = 4,
+
+    /// <summary>
+    /// Weekly
+    /// </summary>
+    [Map("weekly")]
+    Weekly = 3,
+
+    /// <summary>
+    /// Daily
+    /// </summary>
+    [Map("daily")]
+    Daily = 2,
+
+    /// <summary>
+    /// Hourly
+    /// </summary>
+    [Map("hourly")]
+    Hourly = 1
+}
