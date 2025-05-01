3058e7dd478023dc654549115f09f9691d7cd6cc
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:48 +0300

    OkxGridAlgoSubOrder.cs

---
+++ OKXFlash/Grid/Responses/OkxGridAlgoSubOrder.cs
@@ -0,0 +1,175 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Algo Sub Order
+/// </summary>
+public record OkxGridAlgoSubOrder
+{
+    /// <summary>
+    /// Algo order ID
+    /// </summary>
+    [JsonProperty("algoId")]
+    public string AlgoOrderId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Algo client order ID
+    /// </summary>
+    [JsonProperty("algoClOrdId")]
+    public string AlgoClientOrderId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Instrument type
+    /// </summary>
+    [JsonProperty("instType")]
+    public OkxInstrumentType InstrumentType { get; set; }
+
+    /// <summary>
+    /// Instrument ID
+    /// </summary>
+    [JsonProperty("instId")]
+    public string InstrumentId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Algo order type
+    /// </summary>
+    [JsonProperty("algoOrdType")]
+    public OkxGridAlgoOrderType AlgoOrderType { get; set; }
+
+    /// <summary>
+    /// Group ID
+    /// </summary>
+    [JsonProperty("groupId")]
+    public string GroupId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Sub order ID
+    /// </summary>
+    [JsonProperty("ordId")]
+    public long SubOrderId { get; set; }
+
+    /// <summary>
+    /// Created time of sub order
+    /// </summary>
+    [JsonProperty("cTime")]
+    public long CreateTimestamp { get; set; }
+
+    /// <summary>
+    /// Created time of sub order
+    /// </summary>
+    [JsonIgnore]
+    public DateTime CreateTime => CreateTimestamp.ConvertFromMilliseconds();
+
+    /// <summary>
+    /// Updated time of sub order
+    /// </summary>
+    [JsonProperty("uTime")]
+    public long? UpdateTimestamp { get; set; }
+
+    /// <summary>
+    /// Updated time of sub order
+    /// </summary>
+    [JsonIgnore]
+    public DateTime? UpdateTime => UpdateTimestamp?.ConvertFromMilliseconds();
+
+    /// <summary>
+    /// Trade mode
+    /// </summary>
+    [JsonProperty("tdMode")]
+    public OkxTradeMode TradeMode { get; set; }
+
+    /// <summary>
+    /// Currency
+    /// </summary>
+    [JsonProperty("ccy")]
+    public string Currency { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Order type
+    /// </summary>
+    [JsonProperty("ordType")]
+    public OkxTradeOrderType OrderType { get; set; }
+
+    /// <summary>
+    /// Quantity
+    /// </summary>
+    [JsonProperty("sz")]
+    public decimal? Quantity { get; set; }
+
+    /// <summary>
+    /// Algo sub order status
+    /// </summary>
+    [JsonProperty("state")]
+    public OkxGridAlgoSubOrderStatus AlgoSubOrderStatus { get; set; }
+
+    /// <summary>
+    /// Order side
+    /// </summary>
+    [JsonProperty("side")]
+    public OkxTradeOrderSide OrderSide { get; set; }
+
+    /// <summary>
+    /// Price
+    /// </summary>
+    [JsonProperty("px")]
+    public decimal? Price { get; set; }
+
+    /// <summary>
+    /// Fee
+    /// </summary>
+    [JsonProperty("fee")]
+    public decimal? Fee { get; set; }
+
+    /// <summary>
+    /// Fee currency
+    /// </summary>
+    [JsonProperty("feeCcy")]
+    public string FeeCurrency { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Rebate
+    /// </summary>
+    [JsonProperty("rebate")]
+    public decimal? Rebate { get; set; }
+
+    /// <summary>
+    /// Rebate currency
+    /// </summary>
+    [JsonProperty("rebateCcy")]
+    public string RebateCurrency { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Average price
+    /// </summary>
+    [JsonProperty("avgPx")]
+    public decimal? AveragePrice { get; set; }
+
+    /// <summary>
+    /// Accumulated fill quantity
+    /// </summary>
+    [JsonProperty("accFillSz")]
+    public decimal? AccumulatedFillQuantity { get; set; }
+
+    /// <summary>
+    /// Position side
+    /// </summary>
+    [JsonProperty("posSide")]
+    public OkxTradePositionSide PositionSide { get; set; }
+
+    /// <summary>
+    /// Profit and loss
+    /// </summary>
+    [JsonProperty("pnl")]
+    public decimal? ProfitLoss { get; set; }
+
+    /// <summary>
+    /// Contract value
+    /// </summary>
+    [JsonProperty("ctVal")]
+    public decimal? ContractValue { get; set; }
+
+    /// <summary>
+    /// Leverage
+    /// </summary>
+    [JsonProperty("lever")]
+    public int? Leverage { get; set; }
+}
