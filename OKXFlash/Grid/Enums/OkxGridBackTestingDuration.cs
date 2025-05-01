d21a4182bf3dbdc2eb69897f73aca96e3c659c45
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:14 +0300

    OkxGridBackTestingDuration.cs

---
+++ OKXFlash/Grid/Enums/OkxGridBackTestingDuration.cs
@@ -0,0 +1,25 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Back Testing Duration
+/// </summary>
+public enum OkxGridBackTestingDuration
+{
+    /// <summary>
+    /// OneWeek
+    /// </summary>
+    [Map("7D")]
+    OneWeek = 7,
+
+    /// <summary>
+    /// OneMonth
+    /// </summary>
+    [Map("30D")]
+    OneMonth = 30,
+
+    /// <summary>
+    /// SixMonths
+    /// </summary>
+    [Map("180D")]
+    SixMonths = 180,
+}
