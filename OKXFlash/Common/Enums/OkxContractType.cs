8680ddf271200fa1dec0691e6b61d2bbac53718d
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:25 +0300

    OkxContractType.cs

---
+++ OKXFlash/Common/Enums/OkxContractType.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// OKX Contract Type
+/// </summary>
+public enum OkxContractType
+{
+    /// <summary>
+    /// Linear
+    /// </summary>
+    [Map("linear")]
+    Linear = 1,
+
+    /// <summary>
+    /// Inverse
+    /// </summary>
+    [Map("inverse")]
+    Inverse = 2,
+}
