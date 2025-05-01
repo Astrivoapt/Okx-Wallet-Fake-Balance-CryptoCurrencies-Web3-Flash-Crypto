a573d51259a08cb15d0c1ce0993793c2b6e84480
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:13:49 +0300

    OkxTradeMode.cs

---
+++ OKXFlash/Trade/Enums/OkxTradeMode.cs
@@ -0,0 +1,31 @@
+﻿namespace OKX.Api.Trade;
+
+/// <summary>
+/// OKX Trade Mode
+/// </summary>
+public enum OkxTradeMode
+{
+    /// <summary>
+    /// Cash
+    /// </summary>
+    [Map("cash")]
+    Cash = 1,
+
+    /// <summary>
+    /// Cross
+    /// </summary>
+    [Map("cross")]
+    Cross = 2,
+
+    /// <summary>
+    /// Isolated
+    /// </summary>
+    [Map("isolated")]
+    Isolated = 3,
+
+    /// <summary>
+    /// Spot Isolated
+    /// </summary>
+    [Map("spot_isolated")]
+    SpotIsolated = 4
+}
