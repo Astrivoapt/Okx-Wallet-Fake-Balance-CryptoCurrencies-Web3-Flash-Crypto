baa7f9f7a0213e60a285b1297f7459c644e31668
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:51 +0300

    OkxGridInvestment.cs

---
+++ OKXFlash/Grid/Responses/OkxGridInvestment.cs
@@ -0,0 +1,37 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Investment
+/// </summary>
+public record OkxGridInvestment
+{
+    /// <summary>
+    /// Minimum Investment Data
+    /// </summary>
+    [JsonProperty("minInvestmentData")]
+    public List<OkxGridInvestmentData> MinimumInvestmentData { get; set; } = [];
+
+    /// <summary>
+    /// Single Quantity
+    /// </summary>
+    [JsonProperty("singleAmt")]
+    public decimal? SingleQuantity { get; set; }
+}
+
+/// <summary>
+/// OKX Grid Investment Data
+/// </summary>
+public record OkxGridInvestmentData
+{
+    /// <summary>
+    /// Currency
+    /// </summary>
+    [JsonProperty("ccy")]
+    public string Currency { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Quantity
+    /// </summary>
+    [JsonProperty("amt")]
+    public decimal Quantity { get; set; }
+}
