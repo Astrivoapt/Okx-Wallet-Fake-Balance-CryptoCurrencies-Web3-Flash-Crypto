b2038e0708dce0b18e26705d9625206f5e46d50c
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:13:09 +0300

    OkxGridWithdrawIncome.cs

---
+++ OKXFlash/Grid/Responses/OkxGridWithdrawIncome.cs
@@ -0,0 +1,25 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Withdraw Income
+/// </summary>
+public record OkxGridWithdrawIncome
+{
+    /// <summary>
+    /// Algo Order Id
+    /// </summary>
+    [JsonProperty("algoId")]
+    public string AlgoOrderId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Algo Client Order Id
+    /// </summary>
+    [JsonProperty("algoClOrdId")]
+    public string AlgoClientOrderId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Profit
+    /// </summary>
+    [JsonProperty("profit")]
+    public decimal Profit { get; set; }
+}
