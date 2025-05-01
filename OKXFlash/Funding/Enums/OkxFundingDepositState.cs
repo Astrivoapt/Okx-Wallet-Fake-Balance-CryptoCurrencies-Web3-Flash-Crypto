507b23be15f47bdc5b8c2e1aaf0326261f7e5134
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:10:56 +0300

    OkxFundingDepositState.cs

---
+++ OKXFlash/Funding/Enums/OkxFundingDepositState.cs
@@ -0,0 +1,31 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX Deposit State
+/// </summary>
+public enum OkxFundingDepositState
+{
+    /// <summary>
+    /// WaitingForConfirmation
+    /// </summary>
+    [Map("1")]
+    WaitingForConfirmation = 1,
+
+    /// <summary>
+    /// Credited
+    /// </summary>
+    [Map("2")]
+    Credited = 2,
+
+    /// <summary>
+    /// Successful
+    /// </summary>
+    [Map("3")]
+    Successful = 3,
+
+    /// <summary>
+    /// Pending
+    /// </summary>
+    [Map("4")]
+    Pending = 4,
+}
