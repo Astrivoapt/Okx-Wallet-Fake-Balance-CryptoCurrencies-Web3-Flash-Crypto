e34b978c2ba0dbdc6a4260d9a38852fe47eb468c
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:14:08 +0300

    OkxTradeEasyConvertCurrencyList.cs

---
+++ OKXFlash/Trade/Responses/OkxTradeEasyConvertCurrencyList.cs
@@ -0,0 +1,37 @@
+﻿namespace OKX.Api.Trade;
+
+/// <summary>
+/// OKX Easy Convert Currency List
+/// </summary>
+public record OkxTradeEasyConvertCurrencyList
+{
+    /// <summary>
+    /// Currently owned and convertible small currency list
+    /// </summary>
+    [JsonProperty("fromData")]
+    public List<OkxTradeEasyConvertCurrencyFromData> FromData { get; set; } = [];
+
+    /// <summary>
+    /// Type of mainstream currency convert to, e.g. USDT
+    /// </summary>
+    [JsonProperty("toCcy")]
+    public List<string> ToCurrency { get; set; } = [];
+}
+
+/// <summary>
+/// OKX Easy Convert Currency From Data
+/// </summary>
+public record OkxTradeEasyConvertCurrencyFromData
+{
+    /// <summary>
+    /// Type of small payment currency convert from, e.g. BTC
+    /// </summary>
+    [JsonProperty("fromCcy")]
+    public string FromCurrency { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Amount of small payment currency convert from
+    /// </summary>
+    [JsonProperty("fromAmt")]
+    public decimal FromAmount { get; set; }
+}
