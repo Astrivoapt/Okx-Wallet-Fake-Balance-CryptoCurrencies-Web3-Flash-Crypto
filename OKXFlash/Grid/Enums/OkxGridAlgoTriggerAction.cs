a04a1d4822ff5f5d5c23e310d5f3bbbd5d6c9029
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:08 +0300

    OkxGridAlgoTriggerAction.cs

---
+++ OKXFlash/Grid/Enums/OkxGridAlgoTriggerAction.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Algo Trigger Action
+/// </summary>
+public enum OkxGridAlgoTriggerAction
+{
+    /// <summary>
+    /// Start
+    /// </summary>
+    [Map("start")]
+    Start = 1,
+
+    /// <summary>
+    /// Stop
+    /// </summary>
+    [Map("stop")]
+    Stop = 2,
+}
