9d26dc8e7c22929adf26171d8148a848b5a63b57
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:35 +0300

    OkxLiquidationState.cs

---
+++ OKXFlash/Common/Enums/OkxLiquidationState.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// OKX Liquidation State
+/// </summary>
+public enum OkxLiquidationState
+{
+    /// <summary>
+    /// Unfilled
+    /// </summary>
+    [Map("unfilled")]
+    Unfilled = 1,
+
+    /// <summary>
+    /// Filled
+    /// </summary>
+    [Map("filled")]
+    Filled = 2,
+}
