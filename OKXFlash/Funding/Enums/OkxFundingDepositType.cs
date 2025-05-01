421ebc6ec9143a6c1b342dfc925510f2c1cf570b
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:10:57 +0300

    OkxFundingDepositType.cs

---
+++ OKXFlash/Funding/Enums/OkxFundingDepositType.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX Deposit Type
+/// </summary>
+public enum OkxFundingDepositType
+{
+    /// <summary>
+    /// InternalDeposit
+    /// </summary>
+    [Map("3")]
+    InternalDeposit = 3,
+
+    /// <summary>
+    /// BlockchainDeposit
+    /// </summary>
+    [Map("4")]
+    BlockchainDeposit = 4,
+}
