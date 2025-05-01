f4d058aea651c5e2dd35c56e0000c6a09261a8bb
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:37 +0300

    OkxOptionType.cs

---
+++ OKXFlash/Common/Enums/OkxOptionType.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// OKX Option Type
+/// </summary>
+public enum OkxOptionType
+{
+    /// <summary>
+    /// Call
+    /// </summary>
+    [Map("C")]
+    Call = 1,
+
+    /// <summary>
+    /// Put
+    /// </summary>
+    [Map("P")]
+    Put = 2,
+}
