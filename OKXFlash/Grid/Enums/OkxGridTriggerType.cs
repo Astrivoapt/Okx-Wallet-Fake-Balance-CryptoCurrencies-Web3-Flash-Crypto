7046ac26718fcfd639e119c310e4fcfc70b91e0d
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:27 +0300

    OkxGridTriggerType.cs

---
+++ OKXFlash/Grid/Enums/OkxGridTriggerType.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Trigger Type
+/// </summary>
+public enum OkxGridTriggerType
+{
+    /// <summary>
+    /// Auto
+    /// </summary>
+    [Map("auto")]
+    Auto = 1,
+
+    /// <summary>
+    /// Manual
+    /// </summary>
+    [Map("manual")]
+    Manual = 2,
+}
