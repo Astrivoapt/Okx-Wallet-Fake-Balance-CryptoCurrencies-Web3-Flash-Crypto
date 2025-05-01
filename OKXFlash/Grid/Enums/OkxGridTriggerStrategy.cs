60d8bc26f92c03c2efe8646c0a6291efadd98f48
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:26 +0300

    OkxGridTriggerStrategy.cs

---
+++ OKXFlash/Grid/Enums/OkxGridTriggerStrategy.cs
@@ -0,0 +1,25 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Algo Trigger Strategy
+/// </summary>
+public enum OkxGridTriggerStrategy
+{
+    /// <summary>
+    /// Instant
+    /// </summary>
+    [Map("instant")]
+    Instant = 1,
+
+    /// <summary>
+    /// Price
+    /// </summary>
+    [Map("price")]
+    Price = 2,
+
+    /// <summary>
+    /// RSI
+    /// </summary>
+    [Map("rsi")]
+    RSI = 3
+}
