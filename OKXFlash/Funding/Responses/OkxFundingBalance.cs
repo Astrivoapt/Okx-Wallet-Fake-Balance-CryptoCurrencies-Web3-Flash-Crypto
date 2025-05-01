4eef3a5ea0928bcbdf47936ca477e03a72259a16
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:14 +0300

    OkxFundingBalance.cs

---
+++ OKXFlash/Funding/Responses/OkxFundingBalance.cs
@@ -0,0 +1,31 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX Funding Balance
+/// </summary>
+public record OkxFundingBalance
+{
+    /// <summary>
+    /// Currency
+    /// </summary>
+    [JsonProperty("ccy")]
+    public string Currency { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Balance
+    /// </summary>
+    [JsonProperty("bal")]
+    public decimal Balance { get; set; }
+
+    /// <summary>
+    /// Frozen
+    /// </summary>
+    [JsonProperty("frozenBal")]
+    public decimal Frozen { get; set; }
+
+    /// <summary>
+    /// Available
+    /// </summary>
+    [JsonProperty("availBal")]
+    public decimal Available { get; set; }
+}
