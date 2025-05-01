4dde62f9edf7e55c1d79528919f149743870a917
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:50 +0300

    OkxGridComputedMarginBalance.cs

---
+++ OKXFlash/Grid/Responses/OkxGridComputedMarginBalance.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Computed Margin Balance
+/// </summary>
+public record OkxGridComputedMarginBalance
+{
+    /// <summary>
+    /// Leverage
+    /// </summary>
+    [JsonProperty("lever")]
+    public int Leverage { get; set; }
+
+    /// <summary>
+    /// Maximum quantity
+    /// </summary>
+    [JsonProperty("maxAmt")]
+    public decimal MaximumQuantity { get; set; }
+}
