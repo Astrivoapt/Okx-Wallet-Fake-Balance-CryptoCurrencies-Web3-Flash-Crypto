e2f21b01ce8883ff388c4e088d8d3e4d8a82fc66
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:10:02 +0300

    OkxFinancialFlexibleLoanRestClient.cs

---
+++ OKXFlash/Financial/Clients/OkxFinancialFlexibleLoanRestClient.cs
@@ -0,0 +1,17 @@
+﻿namespace OKX.Api.Financial;
+
+/// <summary>
+/// OKX Rest API Flexible Loan Client
+/// </summary>
+public class OkxFinancialFlexibleLoanRestClient(OkxRestApiClient root) : OkxBaseRestClient(root)
+{
+    // Endpoints
+    // TODO: api/v5/finance/flexible-loan/borrow-currencies
+    // TODO: api/v5/finance/flexible-loan/collateral-assets
+    // TODO: api/v5/finance/flexible-loan/max-loan
+    // TODO: api/v5/finance/flexible-loan/max-collateral-redeem-amount
+    // TODO: api/v5/finance/flexible-loan/adjust-collateral
+    // TODO: api/v5/finance/flexible-loan/loan-info
+    // TODO: api/v5/finance/flexible-loan/loan-history
+    // TODO: api/v5/finance/flexible-loan/interest-accrued
+}
