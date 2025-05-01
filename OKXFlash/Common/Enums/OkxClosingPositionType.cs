efbc3aa342c54aa555010b2da7115f932a5ce86f
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:23 +0300

    OkxClosingPositionType.cs

---
+++ OKXFlash/Common/Enums/OkxClosingPositionType.cs
@@ -0,0 +1,37 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// OKX Closing Position Type
+/// </summary>
+public enum OkxClosingPositionType
+{
+    /// <summary>
+    /// ClosePartially
+    /// </summary>
+    [Map("1")]
+    ClosePartially = 1,
+
+    /// <summary>
+    /// CloseAll
+    /// </summary>
+    [Map("2")]
+    CloseAll = 2,
+
+    /// <summary>
+    /// Liquidation
+    /// </summary>
+    [Map("3")]
+    Liquidation = 3,
+
+    /// <summary>
+    /// PartialLiquidation
+    /// </summary>
+    [Map("4")]
+    PartialLiquidation = 4,
+
+    /// <summary>
+    /// ADL
+    /// </summary>
+    [Map("5")]
+    ADL = 5
+}
