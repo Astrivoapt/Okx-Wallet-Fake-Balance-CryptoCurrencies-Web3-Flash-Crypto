cd4651bac6537bb9ecf351754aaa1ba121681d61
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:00 +0300

    OkxGridAlgoOrderStatus.cs

---
+++ OKXFlash/Grid/Enums/OkxGridAlgoOrderStatus.cs
@@ -0,0 +1,31 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Algo Order Status
+/// </summary>
+public enum OkxGridAlgoOrderStatus
+{
+    /// <summary>
+    /// Starting
+    /// </summary>
+    [Map("starting")]
+    Starting = 1,
+
+    /// <summary>
+    /// Running
+    /// </summary>
+    [Map("running")]
+    Running = 2,
+
+    /// <summary>
+    /// Stopping
+    /// </summary>
+    [Map("stopping")]
+    Stopping = 3,
+
+    /// <summary>
+    /// NoClosePosition
+    /// </summary>
+    [Map("no_close_position")]
+    NoClosePosition = 4
+}
