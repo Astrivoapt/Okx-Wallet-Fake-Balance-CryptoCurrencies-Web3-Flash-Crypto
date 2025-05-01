fd56da11296f2c8c8e83b8dda84a20112be51a3d
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:13:27 +0300

    OkxRecurringBuyType.cs

---
+++ OKXFlash/RecurringBuy/Enums/OkxRecurringBuyType.cs
@@ -0,0 +1,13 @@
+﻿namespace OKX.Api.RecurringBuy;
+
+/// <summary>
+/// OKX Recurring Buy Type
+/// </summary>
+public enum OkxRecurringBuyType
+{
+    /// <summary>
+    /// Recurring
+    /// </summary>
+    [Map("recurring")]
+    Recurring = 1,
+}
