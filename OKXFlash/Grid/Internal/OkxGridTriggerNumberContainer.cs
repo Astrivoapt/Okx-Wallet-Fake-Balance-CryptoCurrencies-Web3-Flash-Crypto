d59b88a858546382fe989b5cb0a018aae88c2196
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:33 +0300

    OkxGridTriggerNumberContainer.cs

---
+++ OKXFlash/Grid/Internal/OkxGridTriggerNumberContainer.cs
@@ -0,0 +1,13 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Trigger Number
+/// </summary>
+internal record OkxGridTriggerNumberContainer
+{
+    /// <summary>
+    /// Trigger Number
+    /// </summary>
+    [JsonProperty("triggerNum")]
+    public long? Payload { get; set; }
+}
