cd41f663bbafb69b840ed1823215e657b078ec80
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:50 +0300

    OkxSelfTradePreventionMode.cs

---
+++ OKXFlash/Common/Enums/OkxSelfTradePreventionMode.cs
@@ -0,0 +1,25 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// OKX Self Trade Prevention Mode
+/// </summary>
+public enum OkxSelfTradePreventionMode
+{
+    /// <summary>
+    /// CancelMaker
+    /// </summary>
+    [Map("cancel_maker")]
+    CancelMaker = 1,
+
+    /// <summary>
+    /// CancelTaker
+    /// </summary>
+    [Map("cancel_taker")]
+    CancelTaker = 2,
+
+    /// <summary>
+    /// CancelBoth
+    /// </summary>
+    [Map("cancel_both")]
+    CancelBoth = 3
+}
