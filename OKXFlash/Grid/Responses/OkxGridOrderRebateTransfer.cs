b7c2977d975046564222c519fbde5d6dedd127a3
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:13:00 +0300

    OkxGridOrderRebateTransfer.cs

---
+++ OKXFlash/Grid/Responses/OkxGridOrderRebateTransfer.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// Rebate transfer info
+/// </summary>
+public record OkxGridOrderRebateTransfer
+{
+    /// <summary>
+    /// Rebate amount
+    /// </summary>
+    [JsonProperty("rebate")]
+    public decimal Rebate { get; set; }
+
+    /// <summary>
+    /// Rebate currency
+    /// </summary>
+    [JsonProperty("rebateCcy")]
+    public string RebateCurrency { get; set; } = string.Empty;
+}
