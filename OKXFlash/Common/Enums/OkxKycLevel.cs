edf65c0c4ef980ca8b07776c90a5b158a83721f5
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:33 +0300

    OkxKycLevel.cs

---
+++ OKXFlash/Common/Enums/OkxKycLevel.cs
@@ -0,0 +1,31 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// OKX Kyc Level
+/// </summary>
+public enum OkxKycLevel
+{
+    /// <summary>
+    /// NoVerification
+    /// </summary>
+    [Map("0")]
+    NoVerification = 0,
+
+    /// <summary>
+    /// Level1
+    /// </summary>
+    [Map("1")]
+    Level1 = 1,
+
+    /// <summary>
+    /// Level2
+    /// </summary>
+    [Map("2")]
+    Level2 = 2,
+
+    /// <summary>
+    /// Level3
+    /// </summary>
+    [Map("3")]
+    Level3 = 3
+}
