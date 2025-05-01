3ac54e443fc32eef226d4acc2b36901c87c3f127
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:56 +0300

    AppConfig.cs

---
+++ OKXFlash/Grid/Config/AppConfig.cs
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2024 The Hong-Jin Investment Company.
+ * This file is part of the OKX Trading Server.
+ * File created at 2024-12-05
+ */
+namespace HongJinInvestment.OKX.Server;
+
+public static class AppConfig
+{
+    public static readonly string AppName = "Lampyris 欧意行情交易平台";
+    public static readonly string AppDocFolderName = "Lampyris OKX Trading Server";
+    public static readonly Version Version = new Version(0, 0, 1);
+}
