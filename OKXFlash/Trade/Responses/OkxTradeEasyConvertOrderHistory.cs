46e1619a38bab3bb876b12e86bce6ee086ccb1c7
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:14:11 +0300

    OkxTradeEasyConvertOrderHistory.cs

---
+++ OKXFlash/Trade/Responses/OkxTradeEasyConvertOrderHistory.cs
@@ -0,0 +1,13 @@
+﻿namespace OKX.Api.Trade;
+
+/// <summary>
+/// OKX Easy Convert Order History
+/// </summary>
+public record OkxTradeEasyConvertOrderHistory : OkxTradeEasyConvertOrder
+{
+    /// <summary>
+    /// Account
+    /// </summary>
+    [JsonProperty("acct")]
+    public OkxAccount Account { get; set; }
+}
