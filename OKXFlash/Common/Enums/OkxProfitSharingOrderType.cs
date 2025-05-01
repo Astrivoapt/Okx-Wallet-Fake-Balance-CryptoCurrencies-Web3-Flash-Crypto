c2b1a1674eadae638a4be410cbce836b678dc346
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:45 +0300

    OkxProfitSharingOrderType.cs

---
+++ OKXFlash/Common/Enums/OkxProfitSharingOrderType.cs
@@ -0,0 +1,31 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// OKX Profit Sharing Order Type
+/// </summary>
+public enum OkxProfitSharingOrderType
+{
+    /// <summary>
+    /// Normal order
+    /// </summary>
+    [Map("0")]
+    NormalOrder = 0,
+
+    /// <summary>
+    /// Copy order without profit sharing
+    /// </summary>
+    [Map("1")]
+    CopyOrderWithoutProfitSharing = 1,
+
+    /// <summary>
+    /// Copy order with profit sharing
+    /// </summary>
+    [Map("2")]
+    CopyOrderWithProfitSharing = 2,
+
+    /// <summary>
+    /// Lead order
+    /// </summary>
+    [Map("3")]
+    LeadOrder = 3,
+}
