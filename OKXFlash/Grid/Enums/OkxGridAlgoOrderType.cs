f9e3dd9c2c8b37cf040202bf546f005c84975202
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:01 +0300

    OkxGridAlgoOrderType.cs

---
+++ OKXFlash/Grid/Enums/OkxGridAlgoOrderType.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Algo Order Type
+/// </summary>
+public enum OkxGridAlgoOrderType
+{
+    /// <summary>
+    /// SpotGrid
+    /// </summary>
+    [Map("grid")]
+    SpotGrid = 1,
+
+    /// <summary>
+    /// ContractGrid
+    /// </summary>
+    [Map("contract_grid")]
+    ContractGrid = 2,
+}
