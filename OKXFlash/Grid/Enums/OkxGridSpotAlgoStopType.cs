1208ad2f432c531ce73b3e227ee7e2a7ae1f86b3
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:24 +0300

    OkxGridSpotAlgoStopType.cs

---
+++ OKXFlash/Grid/Enums/OkxGridSpotAlgoStopType.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Spot Algo Stop Type
+/// </summary>
+public enum OkxGridSpotAlgoStopType
+{
+    /// <summary>
+    /// SellBaseCurrency
+    /// </summary>
+    [Map("1")]
+    SellBaseCurrency = 1,
+
+    /// <summary>
+    /// KeepBaseCurrency
+    /// </summary>
+    [Map("2")]
+    KeepBaseCurrency = 2,
+}
