ade4abb4b6b3171d7a5cec2ac138f406fb5a243e
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:13:46 +0300

    OkxTradeEasyConvertOrderStatus.cs

---
+++ OKXFlash/Trade/Enums/OkxTradeEasyConvertOrderStatus.cs
@@ -0,0 +1,25 @@
+﻿namespace OKX.Api.Trade;
+
+/// <summary>
+/// OKX Easy Convert Order Status
+/// </summary>
+public enum OkxTradeEasyConvertOrderStatus
+{
+    /// <summary>
+    /// Running
+    /// </summary>
+    [Map("running")]
+    Running = 1,
+
+    /// <summary>
+    /// Filled
+    /// </summary>
+    [Map("filled")]
+    Filled = 2,
+
+    /// <summary>
+    /// Failed
+    /// </summary>
+    [Map("failed")]
+    Failed = 3,
+}
