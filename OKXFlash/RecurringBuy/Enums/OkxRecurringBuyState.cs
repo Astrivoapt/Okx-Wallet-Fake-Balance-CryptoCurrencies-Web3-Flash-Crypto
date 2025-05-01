5851578430cc8115a75f4e24cc5d0ba1b376252c
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:13:25 +0300

    OkxRecurringBuyState.cs

---
+++ OKXFlash/RecurringBuy/Enums/OkxRecurringBuyState.cs
@@ -0,0 +1,37 @@
+﻿namespace OKX.Api.RecurringBuy;
+
+/// <summary>
+/// OKX Recurring Buy State
+/// </summary>
+public enum OkxRecurringBuyState
+{
+    /// <summary>
+    /// Live
+    /// </summary>
+    [Map("canceled")]
+    Live = 1,
+
+    /// <summary>
+    /// Filled
+    /// </summary>
+    [Map("live")]
+    Filled = 2,
+
+    /// <summary>
+    /// PartiallyFilled
+    /// </summary>
+    [Map("partially_filled")]
+    PartiallyFilled = 3,
+
+    /// <summary>
+    /// Cancelling
+    /// </summary>
+    [Map("filled")]
+    Cancelling = 4,
+
+    /// <summary>
+    /// Canceled
+    /// </summary>
+    [Map("cancelling")]
+    Canceled = 5
+}
