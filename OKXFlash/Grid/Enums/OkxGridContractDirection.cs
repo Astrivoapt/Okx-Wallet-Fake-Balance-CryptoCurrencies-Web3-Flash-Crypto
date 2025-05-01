6619ef72a2fb2c052d3b4ee9b838754149c6dadd
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:19 +0300

    OkxGridContractDirection.cs

---
+++ OKXFlash/Grid/Enums/OkxGridContractDirection.cs
@@ -0,0 +1,25 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Contract Direction
+/// </summary>
+public enum OkxGridContractDirection
+{
+    /// <summary>
+    /// Long
+    /// </summary>
+    [Map("long")]
+    Long = 1,
+
+    /// <summary>
+    /// Short
+    /// </summary>
+    [Map("short")]
+    Short = -1,
+
+    /// <summary>
+    /// Neutral
+    /// </summary>
+    [Map("neutral")]
+    Neutral = 0
+}
