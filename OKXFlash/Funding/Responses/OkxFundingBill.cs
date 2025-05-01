c9533b10302c8c57758521db429c65c64f183a7b
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:16 +0300

    OkxFundingBill.cs

---
+++ OKXFlash/Funding/Responses/OkxFundingBill.cs
@@ -0,0 +1,55 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX Funding Bill
+/// </summary>
+public record OkxFundingBill
+{
+    /// <summary>
+    /// Bill ID
+    /// </summary>
+    [JsonProperty("billId")]
+    public long BillId { get; set; }
+
+    /// <summary>
+    /// Currency
+    /// </summary>
+    [JsonProperty("ccy")]
+    public string Currency { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Client Order ID
+    /// </summary>
+    [JsonProperty("clientId")]
+    public string ClientOrderId { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// Balance Change
+    /// </summary>
+    [JsonProperty("balChg")]
+    public decimal BalanceChange { get; set; }
+
+    /// <summary>
+    /// Balance
+    /// </summary>
+    [JsonProperty("bal")]
+    public decimal Balance { get; set; }
+
+    /// <summary>
+    /// Type
+    /// </summary>
+    [JsonProperty("type")]
+    public OkxFundingBillType Type { get; set; }
+
+    /// <summary>
+    /// Time
+    /// </summary>
+    [JsonProperty("ts")]
+    public long Timestamp { get; set; }
+
+    /// <summary>
+    /// Time
+    /// </summary>
+    [JsonIgnore]
+    public DateTime Time => Timestamp.ConvertFromMilliseconds();
+}
