c6cca28de738e8f816c940530ac43c17008b9295
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:13:44 +0300

    OkxTradeDebtType.cs

---
+++ OKXFlash/Trade/Enums/OkxTradeDebtType.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Trade;
+
+/// <summary>
+/// OKX Debt Type
+/// </summary>
+public enum OkxTradeDebtType
+{
+    /// <summary>
+    /// Cross
+    /// </summary>
+    [Map("cross")]
+    Cross = 1,
+
+    /// <summary>
+    /// Isolated
+    /// </summary>
+    [Map("isolated")]
+    Isolated = 2,
+}
