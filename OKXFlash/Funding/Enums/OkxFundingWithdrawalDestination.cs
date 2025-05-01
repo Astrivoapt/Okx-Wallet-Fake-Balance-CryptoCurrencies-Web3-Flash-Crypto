8c9f96759536f643c7eb1de8bdb86045769a4347
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:03 +0300

    OkxFundingWithdrawalDestination.cs

---
+++ OKXFlash/Funding/Enums/OkxFundingWithdrawalDestination.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX Withdrawal Destination
+/// </summary>
+public enum OkxFundingWithdrawalDestination
+{
+    /// <summary>
+    /// OKX
+    /// </summary>
+    [Map("3")]
+    OKX = 3,
+
+    /// <summary>
+    /// DigitalCurrencyAddress
+    /// </summary>
+    [Map("4")]
+    DigitalCurrencyAddress = 4,
+}
