f20062d3f444a7439b088e2e739470c99e698fb5
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:03 +0300

    OkxGridAlgoSubOrderStatus.cs

---
+++ OKXFlash/Grid/Enums/OkxGridAlgoSubOrderStatus.cs
@@ -0,0 +1,37 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Algo Sub-Order Status
+/// </summary>
+public enum OkxGridAlgoSubOrderStatus
+{
+    /// <summary>
+    /// Live
+    /// </summary>
+    [Map("live")]
+    Live = 1,
+
+    /// <summary>
+    /// Partial
+    /// </summary>
+    [Map("partially_filled")]
+    Partial = 2,
+
+    /// <summary>
+    /// Filled
+    /// </summary>
+    [Map("filled")]
+    Filled = 3,
+
+    /// <summary>
+    /// Cancelling
+    /// </summary>
+    [Map("cancelling")]
+    Cancelling = 4,
+
+    /// <summary>
+    /// Canceled
+    /// </summary>
+    [Map("canceled")]
+    Canceled = 5,
+}
