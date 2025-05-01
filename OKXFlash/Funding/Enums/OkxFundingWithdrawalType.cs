818c53357b1c44c4c51d17e28b1cbdead5f32225
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:06 +0300

    OkxFundingWithdrawalType.cs

---
+++ OKXFlash/Funding/Enums/OkxFundingWithdrawalType.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX Withdrawal Type
+/// </summary>
+public enum OkxFundingWithdrawalType
+{
+    /// <summary>
+    /// InternalDeposit
+    /// </summary>
+    [Map("3")]
+    InternalDeposit = 3,
+
+    /// <summary>
+    /// BlockchainDeposit
+    /// </summary>
+    [Map("4")]
+    BlockchainDeposit = 4,
+}
