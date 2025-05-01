c474ca4f3486422ecfbdb9ac7379f1f6f9b9d30b
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:28 +0300

    OkxInstrumentRuleType.cs

---
+++ OKXFlash/Common/Enums/OkxInstrumentRuleType.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// OKX Instrument Rule Type
+/// </summary>
+public enum OkxInstrumentRuleType
+{
+    /// <summary>
+    /// Normal
+    /// </summary>
+    [Map("normal")]
+    Normal = 1,
+
+    /// <summary>
+    /// PreMarket
+    /// </summary>
+    [Map("pre_market")]
+    PreMarket = 2,
+}
