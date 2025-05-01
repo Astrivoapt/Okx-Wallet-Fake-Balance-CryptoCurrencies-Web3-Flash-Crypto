2319d3fa3c82142b2d89e9698d7e840ff3e2d28a
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:47 +0300

    OkxQuarter.cs

---
+++ OKXFlash/Common/Enums/OkxQuarter.cs
@@ -0,0 +1,31 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// OKX Quarter
+/// </summary>
+public enum OkxQuarter
+{
+    /// <summary>
+    /// Quarter 1
+    /// </summary>
+    [Map("Q1")]
+    Quarter1 = 1,
+
+    /// <summary>
+    /// Quarter 2
+    /// </summary>
+    [Map("Q2")]
+    Quarter2 = 2,
+
+    /// <summary>
+    /// Quarter 3
+    /// </summary>
+    [Map("Q3")]
+    Quarter3 = 3,
+
+    /// <summary>
+    /// Quarter 4
+    /// </summary>
+    [Map("Q4")]
+    Quarter4 = 4,
+}
