4237e338e876014cf2705faa187fe754abf6c72a
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:13:54 +0300

    OkxTradeMassCancelRequest.cs

---
+++ OKXFlash/Trade/Requests/OkxTradeMassCancelRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Trade;
+
+/// <summary>
+/// OKX Mass Cancel Request
+/// </summary>
+public record OkxTradeMassCancelRequest
+{
+    /// <summary>
+    /// Instrument Type
+    /// </summary>
+    [JsonProperty("instType")]
+    public OkxInstrumentType InstrumentType { get; set; }
+
+    /// <summary>
+    /// Instrument Family
+    /// </summary>
+    [JsonProperty("instFamily")]
+    public string InstrumentFamily { get; set; } = string.Empty;
+}
