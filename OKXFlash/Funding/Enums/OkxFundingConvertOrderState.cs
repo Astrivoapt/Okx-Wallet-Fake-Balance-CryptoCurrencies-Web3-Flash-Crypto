dc3ac1db0c4c03d323c309079b5f169341866ce1
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:10:54 +0300

    OkxFundingConvertOrderState.cs

---
+++ OKXFlash/Funding/Enums/OkxFundingConvertOrderState.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX Convert Order State
+/// </summary>
+public enum OkxFundingConvertOrderState
+{
+    /// <summary>
+    /// fullyFilled: success
+    /// </summary>
+    [Map("fullyFilled")]
+    Success = 1,
+
+    /// <summary>
+    /// rejected: failed
+    /// </summary>
+    [Map("rejected")]
+    Failed = 2,
+}
