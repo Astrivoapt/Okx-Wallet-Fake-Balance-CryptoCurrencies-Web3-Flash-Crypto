ee8dbac15b0a1c655be00dd636631f02ea973fad
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:20 +0300

    OkxGridInvestmentType.cs

---
+++ OKXFlash/Grid/Enums/OkxGridInvestmentType.cs
@@ -0,0 +1,31 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Investment Type
+/// </summary>
+public enum OkxGridInvestmentType
+{
+    /// <summary>
+    /// Grid
+    /// </summary>
+    [Map("grid")]
+    Grid = 1,
+
+    /// <summary>
+    /// Quote
+    /// </summary>
+    [Map("quote")]
+    Quote = 2,
+
+    /// <summary>
+    /// Base
+    /// </summary>
+    [Map("base")]
+    Base = 3,
+
+    /// <summary>
+    /// Dual
+    /// </summary>
+    [Map("dual")]
+    Dual = 4
+}
