79865cad557eefed4e49fd0ca312f0fa072b8fd2
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:11:41 +0300

    OkxFundingWithdrawalReceiver.cs

---
+++ OKXFlash/Funding/Responses/OkxFundingWithdrawalReceiver.cs
@@ -0,0 +1,55 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX Withdrawal Receiver
+/// </summary>
+public record OkxFundingWithdrawalReceiver
+{
+    /// <summary>
+    /// Currency
+    /// </summary>
+    [JsonProperty("walletType")]
+    public string WalletType { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Chain
+    /// </summary>
+    [JsonProperty("exchId")]
+    public string ExchangeId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Amount
+    /// </summary>
+    [JsonProperty("rcvrFirstName")]
+    public string ReceiverFirstName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Withdrawal Id
+    /// </summary>
+    [JsonProperty("rcvrLastName")]
+    public string ReceiverLastName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Client Order Id
+    /// </summary>
+    [JsonProperty("rcvrCountry")]
+    public string ReceiverCountry { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Withdrawal Id
+    /// </summary>
+    [JsonProperty("rcvrCountrySubDivision")]
+    public string ReceiverCountrySubDivision { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Client Order Id
+    /// </summary>
+    [JsonProperty("rcvrTownName")]
+    public string ReceiverTownName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Client Order Id
+    /// </summary>
+    [JsonProperty("rcvrTownName")]
+    public string ReceiverStreetName { get; set; } = string.Empty;
+}
