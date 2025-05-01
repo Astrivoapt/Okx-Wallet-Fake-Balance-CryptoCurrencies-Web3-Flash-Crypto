3d820197ab506980f85535262156cbcf6b8e6b1c
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:10:50 +0300

    OKXFundingSocketClient.cs

---
+++ OKXFlash/Funding/Clients/OKXFundingSocketClient.cs
@@ -0,0 +1,13 @@
+﻿namespace OKX.Api.Funding;
+
+/// <summary>
+/// OKX WebSocket Api Funding Account Client
+/// </summary>
+public class OkxFundingSocketClient(OkxWebSocketApiClient root)
+{
+    // Internal
+    internal OkxWebSocketApiClient _ { get; } = root;
+
+    // TODO: Deposit info channel
+    // TODO: Withdrawal info channel
+}
