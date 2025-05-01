ae6bf53f23a5606685aaf24c79e318146d60f5a4
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:12:17 +0300

    OkxGridContractAlgoStopType.cs

---
+++ OKXFlash/Grid/Enums/OkxGridContractAlgoStopType.cs
@@ -0,0 +1,19 @@
+﻿namespace OKX.Api.Grid;
+
+/// <summary>
+/// OKX Grid Contract Algo Stop Type
+/// </summary>
+public enum OkxGridContractAlgoStopType
+{
+    /// <summary>
+    /// MarketCloseAllPositions
+    /// </summary>
+    [Map("1")]
+    MarketCloseAllPositions = 1,
+
+    /// <summary>
+    /// KeepPositions
+    /// </summary>
+    [Map("2")]
+    KeepPositions = 2
+}
