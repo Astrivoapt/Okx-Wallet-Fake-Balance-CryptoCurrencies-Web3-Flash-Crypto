c8b18a648c0249911a83e6df51157fa42f984ee5
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:26 +0300

    OkxDownloadLinkState.cs

---
+++ OKXFlash/Common/Enums/OkxDownloadLinkState.cs
@@ -0,0 +1,25 @@
+﻿namespace OKX.Api.Common;
+
+/// <summary>
+/// OKX Download Link State
+/// </summary>
+public enum OkxDownloadLinkState
+{
+    /// <summary>
+    /// Ongoing
+    /// </summary>
+    [Map("ongoing")]
+    Ongoing = 1,
+
+    /// <summary>
+    /// Finished
+    /// </summary>
+    [Map("finished")]
+    Finished = 2,
+
+    /// <summary>
+    /// Failed
+    /// </summary>
+    [Map("failed")]
+    Failed = 3,
+}
